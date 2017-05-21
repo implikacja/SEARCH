d78e230c3d90f5f52d7a8b970bcdafa1fe73f8c1
Speego <zbigniew.m.andrzejewski@gmail.com>
2017-05-21 14:01:27 +0200

    gitignore

--- searcher/Models/Data.cs
+++ searcher/Models/Data.cs
@@ -21,21 +21,34 @@ namespace searcher.Models {
              (
                 from a in xdoc.Element("PubmedArticleSet").Elements("PubmedArticle")
                 select new Article {
-                    title = (string)a.Element("MedlineCitation").Element("Article").Element("ArticleTitle"),
-                    description = (string)a.Element("MedlineCitation").Element("Article").Element("Abstract"),
-                    date = new DateTime((Int32)a.Element("MedlineCitation").Element("DateCreated").Element("Year"),
-                                        (Int32)a.Element("MedlineCitation").Element("DateCreated").Element("Month"),
-                                        (Int32)a.Element("MedlineCitation").Element("DateCreated").Element("Day")),
-                    authors =
-                    (
-                        from auth in a.Element("MedlineCitation").Element("Article").Element("AuthorList").Elements("Author")
-                        select new Author {
-                            firstName = (string)auth.Element("ForeName"),
-                            lastName = (string)auth.Element("LastName")
-                        }
-                    ).ToArray()
+                    title = (string)a.Element("MedlineCitation").Element("Article").tryToGetXMLValue("ArticleTitle"),
+                    description = (string)a.Element("MedlineCitation").Element("Article").tryToGetXMLValue("Abstract")
+                    //,
+                    //date = new DateTime((Int32)a.Element("MedlineCitation").Element("DateCreated").Element("Year"),
+                    //                    (Int32)a.Element("MedlineCitation").Element("DateCreated").Element("Month"),
+                    //                    (Int32)a.Element("MedlineCitation").Element("DateCreated").Element("Day")),
+                    //authors =
+                    //(
+                    //    from auth in a.Element("MedlineCitation").Element("Article").Element("AuthorList").Elements("Author")
+                    //    select new Author {
+                    //        firstName = (string)auth.Element("ForeName"),
+                    //        lastName = (string)auth.Element("LastName")
+                    //    }
+                    //).ToArray()
                 }
             ).ToList();
+        }
+
+        private static string tryToGetXMLValue(this XElement parentEl, string elementName, string defaultValue = "") {
+
+            if (parentEl != null) {
+                var foundEl = parentEl.Element(elementName);
+                if (foundEl != null) {
+                    return foundEl.Value;
+                }
+            }
+
+            return defaultValue;
         }
 
     }
